e0d7b6e66cd0d651e3e8fc5375fef95b4c80cd56
linghuchong <zyp199702@sina.com>
2020-12-14 23:14:13 +0800

    创建剑法项目

---
+++ huashanjianfa/src/main/java/com/huashan/jianfa/Du9jian.java
@@ -0,0 +1,10 @@
+package com.huashan.jianfa;
+
+public class Du9jian {
+    public static void main(String[] args) {
+        System.out.println("独孤九剑好");
+        System.out.println("独孤九剑好");
+        System.out.println("独孤九剑好");
+        System.out.println("独孤九剑好");
+    }
+}
