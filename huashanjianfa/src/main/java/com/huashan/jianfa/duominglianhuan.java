45e789942d0cbd1c16cede4aea4fd0aa496be8c8
linghuchong <zyp199702@sina.com>
2020-12-15 08:51:16 +0800

    三仙剑

---
+++ huashanjianfa/src/main/java/com/huashan/jianfa/duominglianhuan.java
@@ -0,0 +1,10 @@
+package com.huashan.jianfa;
+
+public class duominglianhuan {
+    public static void main(String[] args) {
+        System.out.println("夺命连环三仙剑");
+        System.out.println("夺命连环三仙剑");
+        System.out.println("夺命连环三仙剑");
+        System.out.println("夺命连环三仙剑");
+}
+}
